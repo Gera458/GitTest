f6ad49c388143c059b87d401b6870d17b1ace77b
Mukul Gera <MukulGe@rsgsys.com>
2020-01-24 11:40:25 +0530

    New comments added

--- src/test/java/Launch1.java
+++ src/test/java/Launch1.java
@@ -1,13 +1,19 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-public class Launch {
+public class Launch1 {
 
-	/*public static void main(String[] args) {
-
+	
+	@Test
+	public void test()
+	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Test_Project\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("https://google.com");
-	}*/
+	}
+	
+	
+	
 }
